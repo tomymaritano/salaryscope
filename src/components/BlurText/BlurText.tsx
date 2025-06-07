bda5e82eb8d3ce97729ec7f23b32b094a60c0ffa
Tomás Maritano <tomasmaritano@Tomass-MacBook-Pro-2.local>
2025-06-06 22:23:47 -0300

    correction eslint

--- src/components/BlurText/BlurText.tsx
+++ src/components/BlurText/BlurText.tsx
@@ -99,12 +99,12 @@ const BlurText: React.FC<BlurTextProps> = ({
       {elements.map((segment, index) => {
         const animateKeyframes = buildKeyframes(fromSnapshot, toSnapshots);
 
-        const spanTransition: Transition = {
+        const spanTransition: Transition & { ease?: (t: number) => number } = {
           duration: totalDuration,
           times,
           delay: (index * delay) / 1000,
+          ease: easing,
         };
-        (spanTransition as any).ease = easing;
 
         return (
           <motion.span
